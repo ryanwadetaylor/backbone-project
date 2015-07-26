f8efdba33a87c7640ab92e4c7b2beaa6c03674e5
Richi Jeffery <kikki_sawn@yahoo.com>
2015-07-26 13:18:43 -0700

    add list-users.js to repo

--- src/views/list-users.js
+++ src/views/list-users.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
 var listUsersTemplate = require('../templates/list-users.hbs');
+var Handlebars = require('hbsfy/runtime');
 
 // App
 
@@ -16,10 +17,19 @@ var ListUsers = Backbone.View.extend({
   render: function () {
     var _this = this;
     var userCollection = this.collection;
+    Handlebars.registerHelper('if_even', function(conditional, options) {
+      if((conditional % 2) == 0) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
+      });
+
 
     // Fetch Collection from Server
     userCollection.fetch().done(function (users) {
       _this.$el.html(listUsersTemplate(users));
+
     });
   }
 });
